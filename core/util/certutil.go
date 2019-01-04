670f21e0feeb84f54f0b26d19dfa166a0f2ced51
medcl <m@medcl.net>
2019-01-04 18:58:29 +0800

    add cert util

---
+++ core/util/certutil.go
@@ -0,0 +1,116 @@
+//https://ericchiang.github.io/post/go-tls/
+package util
+
+import (
+	"crypto/rand"
+	"crypto/rsa"
+	"crypto/tls"
+	"crypto/x509"
+	"crypto/x509/pkix"
+	"encoding/pem"
+	"errors"
+	log "github.com/cihub/seelog"
+	"math/big"
+	"net"
+	"time"
+)
+
+func getRootCert() (rootCert *x509.Certificate, rootKey *rsa.PrivateKey, rootCertPEM []byte) {
+	var err error
+	// generate a new key-pair
+	rootKey, err = rsa.GenerateKey(rand.Reader, 2048)
+	if err != nil {
+		log.Errorf("generating random key: %v", err)
+	}
+
+	rootCertTmpl, err := CertTemplate()
+	if err != nil {
+		log.Errorf("creating cert template: %v", err)
+	}
+	// describe what the certificate will be used for
+	rootCertTmpl.IsCA = true
+	rootCertTmpl.KeyUsage = x509.KeyUsageCertSign | x509.KeyUsageDigitalSignature
+	rootCertTmpl.ExtKeyUsage = []x509.ExtKeyUsage{x509.ExtKeyUsageServerAuth, x509.ExtKeyUsageClientAuth}
+	rootCertTmpl.IPAddresses = []net.IP{net.ParseIP("127.0.0.1")}
+
+	rootCert, rootCertPEM, err = CreateCert(rootCertTmpl, rootCertTmpl, &rootKey.PublicKey, rootKey)
+	if err != nil {
+		log.Errorf("error creating cert: %v", err)
+	}
+	//fmt.Printf("%s\n", rootCertPEM)
+	//fmt.Printf("%#x\n", rootCert.Signature)
+
+	return rootCert, rootKey, rootCertPEM
+}
+
+func getClientCert(rootCert *x509.Certificate, rootKey *rsa.PrivateKey) tls.Certificate {
+
+	// create a key-pair for the client
+	clientKey, err := rsa.GenerateKey(rand.Reader, 2048)
+	if err != nil {
+		log.Errorf("generating random key: %v", err)
+	}
+
+	// create a template for the client
+	clientCertTmpl, err := CertTemplate()
+	if err != nil {
+		log.Errorf("creating cert template: %v", err)
+	}
+	clientCertTmpl.KeyUsage = x509.KeyUsageDigitalSignature
+	clientCertTmpl.ExtKeyUsage = []x509.ExtKeyUsage{x509.ExtKeyUsageClientAuth}
+
+	// the root cert signs the cert by again providing its private key
+	_, clientCertPEM, err := CreateCert(clientCertTmpl, rootCert, &clientKey.PublicKey, rootKey)
+	if err != nil {
+		log.Errorf("error creating cert: %v", err)
+	}
+
+	// encode and load the cert and private key for the client
+	clientKeyPEM := pem.EncodeToMemory(&pem.Block{
+		Type: "RSA PRIVATE KEY", Bytes: x509.MarshalPKCS1PrivateKey(clientKey),
+	})
+	clientTLSCert, err := tls.X509KeyPair(clientCertPEM, clientKeyPEM)
+	if err != nil {
+		log.Errorf("invalid key pair: %v", err)
+	}
+
+	return clientTLSCert
+}
+
+// helper function to create a cert template with a serial number and other required fields
+func CertTemplate() (*x509.Certificate, error) {
+	// generate a random serial number (a real cert authority would have some logic behind this)
+	serialNumberLimit := new(big.Int).Lsh(big.NewInt(1), 128)
+	serialNumber, err := rand.Int(rand.Reader, serialNumberLimit)
+	if err != nil {
+		return nil, errors.New("failed to generate serial number: " + err.Error())
+	}
+
+	tmpl := x509.Certificate{
+		SerialNumber:          serialNumber,
+		Subject:               pkix.Name{Organization: []string{"Infinitbyte.com"}},
+		SignatureAlgorithm:    x509.SHA256WithRSA,
+		NotBefore:             time.Now(),
+		NotAfter:              time.Now().Add(time.Hour), // valid for an hour
+		BasicConstraintsValid: true,
+	}
+	return &tmpl, nil
+}
+
+func CreateCert(template, parent *x509.Certificate, pub interface{}, parentPriv interface{}) (
+	cert *x509.Certificate, certPEM []byte, err error) {
+
+	certDER, err := x509.CreateCertificate(rand.Reader, template, parent, pub, parentPriv)
+	if err != nil {
+		return
+	}
+	// parse the resulting certificate so we can use it again
+	cert, err = x509.ParseCertificate(certDER)
+	if err != nil {
+		return
+	}
+	// PEM encode the certificate (this is a standard TLS encoding)
+	b := pem.Block{Type: "CERTIFICATE", Bytes: certDER}
+	certPEM = pem.EncodeToMemory(&b)
+	return
+}
---
+++ core/util/certutil_test.go
@@ -0,0 +1,104 @@
+/*
+Copyright Medcl (m AT medcl.net)
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+   http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
+package util
+
+import (
+	"crypto/rand"
+	"crypto/rsa"
+	"crypto/tls"
+	"crypto/x509"
+	"encoding/pem"
+	"fmt"
+	log "github.com/cihub/seelog"
+	"github.com/stretchr/testify/assert"
+	"net"
+	"net/http"
+	"net/http/httptest"
+	"net/http/httputil"
+	"testing"
+)
+
+func TestGetCert(t *testing.T) {
+	rootCert, rootKey, rootCertPEM := getRootCert()
+
+	ok := func(w http.ResponseWriter, r *http.Request) { w.Write([]byte("HI!")) }
+
+	// create a key-pair for the server
+	servKey, err := rsa.GenerateKey(rand.Reader, 2048)
+	if err != nil {
+		log.Errorf("generating random key: %v", err)
+	}
+
+	// create a template for the server
+	servCertTmpl, err := CertTemplate()
+	if err != nil {
+		log.Errorf("creating cert template: %v", err)
+	}
+	servCertTmpl.KeyUsage = x509.KeyUsageDigitalSignature
+	servCertTmpl.ExtKeyUsage = []x509.ExtKeyUsage{x509.ExtKeyUsageServerAuth}
+	servCertTmpl.IPAddresses = []net.IP{net.ParseIP("127.0.0.1")}
+
+	// create a certificate which wraps the server's public key, sign it with the root private key
+	_, servCertPEM, err := CreateCert(servCertTmpl, rootCert, &servKey.PublicKey, rootKey)
+	if err != nil {
+		log.Errorf("error creating cert: %v", err)
+	}
+
+	// provide the private key and the cert
+	servKeyPEM := pem.EncodeToMemory(&pem.Block{
+		Type: "RSA PRIVATE KEY", Bytes: x509.MarshalPKCS1PrivateKey(servKey),
+	})
+	servTLSCert, err := tls.X509KeyPair(servCertPEM, servKeyPEM)
+	if err != nil {
+		log.Errorf("invalid key pair: %v", err)
+	}
+
+	// create a pool of trusted certs
+	certPool := x509.NewCertPool()
+	certPool.AppendCertsFromPEM(rootCertPEM)
+
+	clientTLSCert := getClientCert(rootCert, rootKey)
+
+	authedClient := &http.Client{
+		Transport: &http.Transport{
+			TLSClientConfig: &tls.Config{
+				RootCAs:      certPool,
+				Certificates: []tls.Certificate{clientTLSCert},
+			},
+		},
+	}
+
+	s := httptest.NewUnstartedServer(http.HandlerFunc(ok))
+	s.TLS = &tls.Config{
+		Certificates: []tls.Certificate{servTLSCert},
+		ClientAuth:   tls.RequireAndVerifyClientCert,
+		ClientCAs:    certPool,
+	}
+	s.StartTLS()
+	resp, err := authedClient.Get(s.URL)
+	s.Close()
+	assert.Equal(t, nil, err)
+
+	dump, err := httputil.DumpResponse(resp, true)
+	if err != nil {
+		log.Errorf("could not dump response: %v", err)
+	}
+	fmt.Printf("%s\n", dump)
+	assert.Equal(t, 200, resp.StatusCode)
+	assert.Equal(t, 3, int(resp.ContentLength))
+
+}
