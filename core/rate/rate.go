9129205d7e9015b94adb3166989fb47e04d5bfde
medcl <m@medcl.net>
2021-04-07 17:37:53 +0800

    refactor rate limiter

--- core/rate/rate.go
+++ core/rate/rate.go
@@ -6,12 +6,13 @@ package rate
 import (
 	"sync"
 	"golang.org/x/time/rate"
+	"time"
 )
 
 var raters = make(map[string]map[string]*rate.Limiter)
 var mu sync.Mutex
 
-func GetRaterWithDefine(category,key string, maxQPS int) *rate.Limiter {
+func GetRateLimiterPerSecond(category,key string, maxQPS int) *rate.Limiter {
 	mu.Lock()
 	defer mu.Unlock()
 
@@ -23,6 +24,29 @@ func GetRaterWithDefine(category,key string, maxQPS int) *rate.Limiter {
 	limiter, exists := raters[category][key]
 	if !exists {
 		limiter = rate.NewLimiter(rate.Limit(maxQPS), maxQPS)
+		raters[category][key] = limiter
+	}
+
+	return limiter
+}
+
+func GetRateLimiter(category,key string,limit,burstLimit int,interval time.Duration) *rate.Limiter {
+	mu.Lock()
+	defer mu.Unlock()
+
+	if burstLimit<limit{
+		burstLimit=limit
+	}
+
+	_,ok:=raters[category]
+	if !ok{
+		raters[category]=map[string]*rate.Limiter{}
+	}
+
+	limiter, exists := raters[category][key]
+	if !exists {
+		rt := rate.Every(interval / time.Duration(limit))
+		limiter = rate.NewLimiter(rt, burstLimit)
 		raters[category][key] = limiter
 	}
 
