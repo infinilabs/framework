f76dc4c62d128a15592331dab113e58688fb3954
medcl <m@medcl.net>
2021-08-20 16:24:06 +0800

    add redis queue

---
+++ modules/queue/queue.go
@@ -0,0 +1,50 @@
+/* ©INFINI, All Rights Reserved.
+ * mail: contact#infini.ltd */
+
+package queue
+
+import (
+	"infini.sh/framework/core/config"
+	"infini.sh/framework/core/env"
+	"infini.sh/framework/core/queue"
+)
+
+type QueueModule struct {
+	configs []QueueConfig
+}
+
+//func New()module.Module  {
+//	return &QueueModule{}
+//}
+
+type QueueConfig struct {
+	Name string `config:"name"`
+	Type string `config:"type"`
+}
+
+func (module *QueueModule) Name() string {
+	return "queue"
+}
+
+func (module *QueueModule) Setup(cfg *config.Config) {
+	module.configs=[]QueueConfig{}
+	ok,err:=env.ParseConfig("queue", &module.configs)
+	if ok&&err!=nil{
+		panic(err)
+	}
+}
+
+func (module *QueueModule) Start() error {
+
+	if module.configs!=nil&&len(module.configs)>0{
+		for _,v:=range module.configs{
+			queue.IniQueue(v.Name,v.Type)
+		}
+	}
+
+	return nil
+}
+
+func (module *QueueModule) Stop() error {
+	return nil
+}
