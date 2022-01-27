08351e79f54062e29ed41b081ba3140bd2ef4456
medcl <m@medcl.net>
2022-01-27 10:48:53 +0800

    add number format util

--- core/util/numers.go
+++ core/util/numers.go
@@ -3,6 +3,12 @@
 
 package util
 
+import (
+	"math"
+	"strconv"
+	"strings"
+)
+
 func MinInt64(x int64, y int64)int64{
 	if x==0{
 		return y
@@ -33,4 +39,141 @@ func MaxInt64(x int64, y int64)int64{
 	}else{
 		return y
 	}
+}
+
+
+
+// credit to https://github.com/DeyV/gotools/blob/master/numbers.go
+func RoundPrec(x float64, prec int) float64 {
+	if math.IsNaN(x) || math.IsInf(x, 0) {
+		return x
+	}
+
+	sign := 1.0
+	if x < 0 {
+		sign = -1
+		x *= -1
+	}
+
+	var rounder float64
+	pow := math.Pow(10, float64(prec))
+	intermed := x * pow
+	_, frac := math.Modf(intermed)
+
+	if frac >= 0.5 {
+		rounder = math.Ceil(intermed)
+	} else {
+		rounder = math.Floor(intermed)
+	}
+
+	return rounder / pow * sign
+}
+
+func NumberFormat(number float64, decimals int, decPoint, thousandsSep string) string {
+	if math.IsNaN(number) || math.IsInf(number, 0) {
+		number = 0
+	}
+
+	var ret string
+	var negative bool
+
+	if number < 0 {
+		number *= -1
+		negative = true
+	}
+
+	d, fract := math.Modf(number)
+
+	if decimals <= 0 {
+		fract = 0
+	} else {
+		pow := math.Pow(10, float64(decimals))
+		fract = RoundPrec(fract*pow, 0)
+	}
+
+	if thousandsSep == "" {
+		ret = strconv.FormatFloat(d, 'f', 0, 64)
+	} else if d >= 1 {
+		var x float64
+		for d >= 1 {
+			d, x = math.Modf(d / 1000)
+			x = x * 1000
+			ret = strconv.FormatFloat(x, 'f', 0, 64) + ret
+			if d >= 1 {
+				ret = thousandsSep + ret
+			}
+		}
+	} else {
+		ret = "0"
+	}
+
+	fracts := strconv.FormatFloat(fract, 'f', 0, 64)
+
+	// "0" pad left
+	for i := len(fracts); i < decimals; i++ {
+		fracts = "0" + fracts
+	}
+
+	ret += decPoint + fracts
+
+	if negative {
+		ret = "-" + ret
+	}
+	return ret
+}
+
+func RoundInt(input float64) int {
+	var result float64
+
+	if input < 0 {
+		result = math.Ceil(input - 0.5)
+	} else {
+		result = math.Floor(input + 0.5)
+	}
+
+	// only interested in integer, ignore fractional
+	i, _ := math.Modf(result)
+
+	return int(i)
+}
+
+func FormatNumber(input float64) string {
+	x := RoundInt(input)
+	xFormatted := NumberFormat(float64(x), 2, ".", ",")
+	return xFormatted
+}
+
+// 	fmt.Println(num, " = ", NearestThousandFormat(float64(num)))
+//10 = 10
+//100 = 100
+//1000 = 1.0k
+//10000 = 10.0k
+//3.12345678912e+09 = 3.1b
+//999.4 = 999
+//-372712 = -372.7k
+//-37271922 = -37.2m
+//-198 = -198
+func NearestThousandFormat(num float64) string {
+
+	if math.Abs(num) < 999.5 {
+		xNum := FormatNumber(num)
+		xNumStr := xNum[:len(xNum)-3]
+		return string(xNumStr)
+	}
+
+	xNum := FormatNumber(num)
+	// first, remove the .00 then convert to slice
+	xNumStr := xNum[:len(xNum)-3]
+	xNumCleaned := strings.Replace(xNumStr, ",", " ", -1)
+	xNumSlice := strings.Fields(xNumCleaned)
+	count := len(xNumSlice) - 2
+	unit := [4]string{"k", "m", "b", "t"}
+	xPart := unit[count]
+
+	afterDecimal := ""
+	if xNumSlice[1][0] != 0 {
+		afterDecimal = "." + string(xNumSlice[1][0])
+	}
+	final := xNumSlice[0] + afterDecimal + xPart
+	return final
 }
