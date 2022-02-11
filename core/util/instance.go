0dc876c1465604cf2b3a844fdb6f2c616f9bf364
medcl <m@medcl.net>
2022-02-11 17:44:06 +0800

    remove lock when process is not there

--- core/util/instance.go
+++ core/util/instance.go
@@ -6,15 +6,57 @@ import (
 	"os"
 	"path"
 	"path/filepath"
+	"syscall"
 )
 
 var locked bool
 var file string
 
+func PidExists(pid int32) (bool, error) {
+	if pid <= 0 {
+		return false, fmt.Errorf("invalid pid %v", pid)
+	}
+	proc, err := os.FindProcess(int(pid))
+	if err != nil {
+		return false, err
+	}
+	err = proc.Signal(syscall.Signal(0))
+	if err == nil {
+		return true, nil
+	}
+	if err.Error() == "os: process already finished" {
+		return false, nil
+	}
+	errno, ok := err.(syscall.Errno)
+	if !ok {
+		return false, err
+	}
+	switch errno {
+	case syscall.ESRCH:
+		return false, nil
+	case syscall.EPERM:
+		return true, nil
+	}
+	return false, err
+}
+
+
 // CheckInstanceLock make sure there is not a lock placed before check, and place a lock after check
 func CheckInstanceLock(p string) {
 	file = path.Join(p, ".lock")
 	if FileExists(file) {
+		pidBytes,err:=FileGetContent(file)
+		if err==nil&&len(pidBytes)>0{
+			pid,err:=ToInt(string(pidBytes))
+			if err==nil&&pid>0{
+				exists,err:=PidExists(int32(pid))
+				if err==nil&&!exists{
+					ClearInstanceLock()
+					log.Debugf("pid [%v] exists, but process is gone, remove the lock file and continue",pid)
+					return
+				}
+			}
+		}
 		log.Errorf("lock file:%s exists, seems one instance is already running, please remove it or set `allow_multi_instance` to `true`", file)
 		log.Flush()
 		os.Exit(1)
