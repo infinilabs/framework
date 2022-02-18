34eb9fdd722d9fcdd32baef6e74f538fa8ad5afb
medcl <m@medcl.net>
2022-02-18 15:48:53 +0800

    add consumer_has_lag

---
+++ core/conditions/consumer_has_lag.go
@@ -0,0 +1,67 @@
+package conditions
+
+import (
+	"fmt"
+	"infini.sh/framework/core/queue"
+	"infini.sh/framework/core/util"
+)
+
+type ConsumerHasLag struct {
+	Queue string
+	Group string
+	Consumer string
+}
+
+func NewConsumerHasLagCondition(fields  map[string]interface{}) (c ConsumerHasLag) {
+	c = ConsumerHasLag{}
+	if len(fields)>0{
+		d:=util.MapStr(fields)
+		if d.SafetyHasKey("queue"){
+			c.Queue=d["queue"].(string)
+			if d.SafetyHasKey("group") && d.SafetyHasKey("cosumer"){
+				c.Group=d["group"].(string)
+				c.Consumer=d["consumer"].(string)
+			}
+			return c
+		}
+	}
+	return c
+}
+
+func (c ConsumerHasLag) Check(event ValuesMap) bool {
+	if c.Queue!=""{
+		qConfig,ok:=queue.GetConfig(c.Queue)
+		if ok{
+			latestProduceOffset:=queue.LatestOffset(qConfig)
+			if c.Group!=""&&c.Consumer!=""{
+				cConfig,ok:=queue.GetConsumerConfig(c.Queue,c.Group,c.Consumer)
+				if ok{
+					consumerOffset,err:=queue.GetOffset(qConfig,cConfig)
+					if err!=nil{
+						panic(err)
+					}
+					if consumerOffset!=latestProduceOffset{
+						return true
+					}
+				}
+			}else{
+				_,seg,pos:=queue.GetEarlierOffsetByQueueID(c.Queue)
+				offset:=fmt.Sprintf("%v,%v",seg,pos)
+
+				if latestProduceOffset!=offset{
+					return true
+				}
+			}
+		}
+	}
+
+	return false
+}
+
+func (c ConsumerHasLag) Name() string {
+	return "consumer_has_lag"
+}
+
+func (c ConsumerHasLag) String() string {
+	return fmt.Sprintf("[%v][%v] on [%v]", c.Group,c.Consumer,c.Queue)
+}
---
+++ core/conditions/consumer_has_lag_test.go
@@ -0,0 +1,22 @@
+/* Copyright © INFINI Ltd. All rights reserved.
+ * web: https://infinilabs.com
+ * mail: hello#infini.ltd */
+
+package conditions
+
+import (
+	"src/github.com/stretchr/testify/assert"
+	"testing"
+)
+
+func TestNewConsumerHasLagCondition(t *testing.T) {
+	config := Config{
+		ConsumerHasLag: &Fields{fields: map[string]interface{}{
+			"queue": "myqueue",
+			"group": "group",
+			"consumer": "myconsumer1",
+		}},
+	}
+	_, err := NewCondition(&config)
+	assert.Error(t, err)
+}
