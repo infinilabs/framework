f1796fbcb30c881d6a258c66a30b5f716b282243
medcl <m@medcl.net>
2022-03-17 16:35:56 +0800

    catch error for task moduel

--- core/task/task.go
+++ core/task/task.go
@@ -10,11 +10,12 @@ import (
 
 var scheduleTasks = []ScheduleTask{}
 var taskLock sync.RWMutex
+
 type ScheduleTask struct {
 	Description string
-	Type string
-	Interval string
-	Task func()
+	Type        string
+	Interval    string
+	Task        func()
 }
 
 func RegisterScheduleTask(task ScheduleTask) {
@@ -25,7 +26,7 @@ func RegisterScheduleTask(task ScheduleTask) {
 
 var quit = make(chan struct{})
 
-func RunTasks()  {
+func RunTasks() {
 	go func() {
 		defer func() {
 			if !global.Env().IsDebug {
@@ -46,16 +47,34 @@ func RunTasks()  {
 		ticker := time.NewTicker(10 * time.Second)
 		for {
 			select {
-			case <- ticker.C:
+			case <-ticker.C:
 				taskLock.RLock()
-				for _,task:=range scheduleTasks{
-					if global.Env().IsDebug{
-						log.Tracef("task: %s, %v, %v",task.Description,task.Type,task.Interval)
+				for _, task := range scheduleTasks {
+					if global.Env().IsDebug {
+						log.Tracef("task: %s, %v, %v", task.Description, task.Type, task.Interval)
 					}
-					go task.Task()
+					go func() {
+						defer func() {
+							if !global.Env().IsDebug {
+								if r := recover(); r != nil {
+									var v string
+									switch r.(type) {
+									case error:
+										v = r.(error).Error()
+									case runtime.Error:
+										v = r.(runtime.Error).Error()
+									case string:
+										v = r.(string)
+									}
+									log.Error("error on run task,", v)
+								}
+							}
+						}()
+						task.Task()
+					}()
 				}
 				taskLock.RUnlock()
-			case <- quit:
+			case <-quit:
 				ticker.Stop()
 				return
 			}
