3f360959d651ca63f61f9a8dd968f812ca277ec3
liugq <silenceqi@hotmail.com>
2022-05-27 12:26:20 +0800

    add cluster auth

--- core/api/auth.go
+++ core/api/auth.go
@@ -142,3 +142,26 @@ func (handler Handler) RequirePermission(h httprouter.Handle, permissions ...str
 		h(w, r, ps)
 	}
 }
+
+func (handler Handler) RequireClusterPermission(h httprouter.Handle, permissions ...string) httprouter.Handle {
+	return func(w http.ResponseWriter, r *http.Request, ps httprouter.Params) {
+
+		if authEnabled {
+			id := ps.ByName("id")
+			claims, err := rbac.ValidateLogin(r.Header.Get("Authorization"))
+			if err != nil {
+				handler.WriteError(w, err.Error(), http.StatusUnauthorized)
+				return
+			}
+			r = r.WithContext(rbac.NewUserContext(r.Context(), claims))
+			hasAllPrivilege, clusterIDs := rbac.GetCurrentUserCluster(r)
+			if !hasAllPrivilege && (len(clusterIDs) == 0 || !util.StringInArray(clusterIDs, id)) {
+				w.WriteHeader(http.StatusForbidden)
+				w.Write([]byte(http.StatusText(http.StatusForbidden)))
+				return
+			}
+		}
+
+		h(w, r, ps)
+	}
+}
