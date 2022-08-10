e041da68c53596bb1df0b5ab6c7775a613d31196
liugq <silenceqi@hotmail.com>
2022-08-10 19:12:40 +0800

    update module agent

---
+++ modules/agent/client.go
@@ -0,0 +1,57 @@
+/* Copyright © INFINI Ltd. All rights reserved.
+ * Web: https://infinilabs.com
+ * Email: hello#infini.ltd */
+
+package agent
+
+import (
+	"context"
+	"fmt"
+	"infini.sh/framework/core/util"
+	"net/http"
+)
+
+type Client struct {
+}
+
+func (client *Client) EnableTaskToNode(ctx context.Context, agentBaseURL string, nodeUUID string) error {
+	req := &util.Request{
+		Method: http.MethodGet,
+		Url: fmt.Sprintf("%s/task/%s/_enable", agentBaseURL, nodeUUID),
+		Context: ctx,
+	}
+	resBody := map[string]interface{}{}
+	err := client.doRequest(req, &resBody)
+	if err != nil {
+		return err
+	}
+	if resBody["result"] != "success" {
+		return fmt.Errorf("%v", resBody["error"])
+	}
+	return nil
+}
+
+func (client *Client) DisableTaskToNode(ctx context.Context, agentBaseURL string, nodeUUID string) error {
+	req := &util.Request{
+		Method: http.MethodGet,
+		Url: fmt.Sprintf("%s/task/%s/_disable", agentBaseURL, nodeUUID),
+		Context: ctx,
+	}
+	resBody := map[string]interface{}{}
+	err := client.doRequest(req, &resBody)
+	if err != nil {
+		return err
+	}
+	if resBody["result"] != "success" {
+		return fmt.Errorf("%v", resBody["error"])
+	}
+	return nil
+}
+
+func (client *Client) doRequest(req *util.Request, respObj interface{}) error{
+	result, err := util.ExecuteRequest(req)
+	if err != nil {
+		return err
+	}
+	return util.FromJSONBytes(result.Body, respObj)
+}
