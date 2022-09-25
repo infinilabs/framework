9a2afbdbf9b81d83509931b581f272828f8a31aa
chengkai <chengkai@infini.ltd>
2022-09-25 20:48:10 +0800

    bugfix/cpu_metric (#48)
    remove src prefix
    
    fix float data can not be handle by console
    
    change cpu metric to percent
    
    Co-authored-by: Cheng Kai <chengkai@infini.ltd>

--- modules/metrics/host/cpu/cpu.go
+++ modules/metrics/host/cpu/cpu.go
@@ -11,7 +11,7 @@ import (
 	"infini.sh/framework/core/config"
 	"infini.sh/framework/core/event"
 	"infini.sh/framework/core/util"
-	"time"
+	"strconv"
 )
 
 type MetricType string
@@ -25,12 +25,21 @@ const TypeLoad = MetricType("load")
 type Metric struct {
 	Enabled bool     `config:"enabled"`
 	Metrics []string `config:"metrics"`
+	prevCounter cpuCounter
+}
+
+type cpuCounter struct {
+	preIOWait float64
+	preSystem float64
+	preIdle   float64
+	preUser   float64
 }
 
 func New(cfg *config.Config) (*Metric, error) {
 
 	me := &Metric{
 		Enabled: true,
+		prevCounter: cpuCounter{},
 	}
 
 	err := cfg.Unpack(&me)
@@ -47,55 +56,89 @@ func (m *Metric) Collect() error {
 	cpuTotal, err := cpu.Times(false)
 	if err != nil {
 		log.Errorf("error %v", err)
-	}
-	if len(cpuTotal) == 0 {
-		log.Error("could not get CPUs", err)
-	}
-	cpuStats := cpuTotal[0]
-
-	mapStr := util.MapStr{}
-	for _, metric := range m.Metrics {
-		switch MetricType(metric) {
-		case TypeIoWait:
-			mapStr.Put(string(TypeIoWait), cpuStats.Iowait)
-		case TypeLoad:
-			v, err := load.Avg()
-			if err != nil {
-				return err
-			}
-			mapStr.Put(string(TypeLoad), util.MapStr{
-				"load1":  v.Load1,
-				"load5":  v.Load5,
-				"load15": v.Load15,
-			})
-		case TypeSystem:
-			mapStr.Put(string(TypeSystem), cpuStats.System)
-		case TypeIdle:
-			mapStr.Put(string(TypeIdle), cpuStats.Idle)
-		case TypeUser:
-			mapStr.Put(string(TypeUser), cpuStats.User)
-		}
-	}
-	percents, err := cpu.Percent(time.Millisecond*100, false) //cpu use percent in 100ms
-	if err != nil {
-		log.Error("get cpu use percent failed: %v", err)
 		return nil
 	}
-	var percent float64
-	if len(percents) > 0 {
-		percent = percents[0]
+	if len(cpuTotal) == 0 {
+		log.Error("could not get CPUs")
+		return nil
 	}
-	mapStr.Put("used_percent", percent) //cpu使用率
-	return event.Save(event.Event{
-		Metadata: event.EventMetadata{
-			Category: "host",
-			Name:     "cpu_usage",
-			Datatype: "gauge",
-		},
-		Fields: util.MapStr{
-			"host": util.MapStr{
-				"cpu_usage": mapStr,
+	cpuStats := cpuTotal[0]
+	if m.prevCounter != (cpuCounter{}) {
+		log.Debugf("Collect CPU metrics, origin: %v", util.MustToJSON(cpuStats))
+		mapStr := util.MapStr{}
+		for _, metric := range m.Metrics {
+			switch MetricType(metric) {
+			case TypeIoWait:
+				iowait := (cpuStats.Iowait - m.prevCounter.preIOWait) / 10.00 * 100.00 //interval: 10s. convert to %
+				retIOWait := KeepZero(iowait)
+				mapStr.Put(string(TypeIoWait), retIOWait)
+			case TypeSystem:
+				system := (cpuStats.System-m.prevCounter.preSystem)/10.00*100.00 //interval: 10s. convert to %
+				retSystem := KeepZero(system)
+				mapStr.Put(string(TypeSystem), retSystem)
+			case TypeIdle:
+				idle := 100.00-(cpuStats.Idle-m.prevCounter.preIdle)/10.00 //interval: 10s. convert to %
+				retIdle := KeepZero(idle)
+				mapStr.Put(string(TypeIdle), retIdle)
+			case TypeUser:
+				user := (cpuStats.User-m.prevCounter.preUser)/10.00*100.00
+				retIdle := KeepZero(user)
+				mapStr.Put(string(TypeUser), retIdle) //interval: 10s. convert to %
+			case TypeLoad:
+				v, err := load.Avg()
+				if err != nil {
+					log.Error(err)
+					continue
+				}
+				retL1 := KeepZero(v.Load1)
+				retL5 := KeepZero(v.Load5)
+				retL15 := KeepZero(v.Load15)
+				mapStr.Put(string(TypeLoad), util.MapStr{
+					"load1":  retL1,
+					"load5":  retL5,
+					"load15": retL15,
+				})
+			}
+		}
+		percents, err := cpu.Percent(0, false) //cpu use percent in 100ms
+		if err != nil {
+			log.Error("get cpu used percent failed: %v", err)
+		}
+		var percent float64
+		if len(percents) > 0 {
+			percent = percents[0]
+		}
+		mapStr.Put("used_percent", percent)
+		log.Debugf("Collect CPU metrics: %v", util.MustToJSON(mapStr))
+		err = event.Save(event.Event{
+			Metadata: event.EventMetadata{
+				Category: "host",
+				Name:     "cpu_usage",
+				Datatype: "gauge",
 			},
-		},
-	})
+			Fields: util.MapStr{
+				"host": util.MapStr{
+					"cpu_usage": mapStr,
+				},
+			},
+		})
+		if err != nil {
+			log.Error(err)
+		}
+	}
+
+	m.prevCounter.preIdle = cpuStats.Idle
+	m.prevCounter.preSystem = cpuStats.System
+	m.prevCounter.preIOWait = cpuStats.Iowait
+	m.prevCounter.preUser = cpuStats.User
+	return nil
+}
+
+type KeepZero float64
+
+func (f KeepZero) MarshalJSON() ([]byte, error) {
+	if float64(f) == float64(int(f)) {
+		return []byte(strconv.FormatFloat(float64(f), 'f', 1, 32)), nil
+	}
+	return []byte(strconv.FormatFloat(float64(f), 'f', -1, 32)), nil
 }
