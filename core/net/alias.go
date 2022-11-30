cad6ccfd1748359fa58fb53f63f18a5948c7aea0
medcl <m@medcl.net>
2022-11-30 18:20:58 +0800

    update logging level

--- core/net/alias.go
+++ core/net/alias.go
@@ -7,7 +7,7 @@ import (
 )
 
 func checkPermission() {
-	log.Debug("to continue use net alias, you need to run as root user.")
+	log.Trace("to continue use net alias, you need to run as root user.")
 	if !util.HasSudoPermission() {
 		panic(errors.New("root permission are required."))
 	}
