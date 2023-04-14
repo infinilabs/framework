0e6f7dd8c10785e3d797b3971c02509809549718
sunjiacheng <sunjiacheng@infini.ltd>
2023-04-14 09:03:32 +0800

    [core][task] update model (#277)
    [core][task] update model
    
    Co-authored-by: Kassian Sun <kassiansun@outlook.com>

--- core/task/model.go
+++ core/task/model.go
@@ -13,16 +13,18 @@ import (
 type Task struct {
 	orm.ORMObjectBase
 
-	ParentId          []string    `json:"parent_id,omitempty" elastic_mapping:"parent_id: { type: keyword }"`
-	StartTimeInMillis int64       `json:"start_time_in_millis" elastic_mapping:"start_time_in_millis: { type: long }"`
-	Cancellable       bool        `json:"cancellable" elastic_mapping:"cancellable: { type: boolean }"`
-	Runnable          bool        `json:"runnable" elastic_mapping:"runnable: { type: boolean }"`
-	Metadata          Metadata    `json:"metadata" elastic_mapping:"metadata: { type: object }"`
-	Status            string      `json:"status"  elastic_mapping:"status: { type: keyword }"`
-	Description       string      `json:"description,omitempty" elastic_mapping:"description: { type: text }"`
-	Config            interface{} `json:"config" elastic_mapping:"config:{type: object,enabled:false }"`
-	CompletedTime     *time.Time  `json:"completed_time,omitempty" elastic_mapping:"completed_time: { type: date }"`
-	RetryTimes        int         `json:"retry_times,omitempty" elastic_mapping:"retry_times: { type: integer }"`
+	ParentId          []string   `json:"parent_id,omitempty" elastic_mapping:"parent_id: { type: keyword }"`
+	StartTimeInMillis int64      `json:"start_time_in_millis" elastic_mapping:"start_time_in_millis: { type: long }"`
+	Cancellable       bool       `json:"cancellable" elastic_mapping:"cancellable: { type: boolean }"`
+	Runnable          bool       `json:"runnable" elastic_mapping:"runnable: { type: boolean }"`
+	Metadata          Metadata   `json:"metadata" elastic_mapping:"metadata: { type: object }"`
+	Status            string     `json:"status"  elastic_mapping:"status: { type: keyword }"`
+	Description       string     `json:"description,omitempty" elastic_mapping:"description: { type: text }"`
+	ConfigString      string     `json:"config_string" elastic_mapping:"config_string:{ type: text }"`
+	CompletedTime     *time.Time `json:"completed_time,omitempty" elastic_mapping:"completed_time: { type: date }"`
+	RetryTimes        int        `json:"retry_times,omitempty" elastic_mapping:"retry_times: { type: integer }"`
+	// DEPRECATED: used by old tasks
+	Config_ interface{} `json:"config,omitempty" elastic_mapping:"config:{type: object,enabled:false }"`
 }
 
 type Metadata struct {
