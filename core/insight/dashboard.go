e407cef9449035c146bf4243cfc0d918adc16ba9
silenceqi <silenceqi@infini.ltd>
2023-05-08 15:56:00 +0800

    set mapping enabled to false (#307)
    set mapping enabled to false
    
    Co-authored-by: liugq <silenceqi@hotmail.com>

--- core/insight/dashboard.go
+++ core/insight/dashboard.go
@@ -13,13 +13,13 @@ type Dashboard struct {
 	ClusterId    string `json:"cluster_id" elastic_mapping:"cluster_id: { type: keyword }"`
 	IndexPattern string `json:"index_pattern" elastic_mapping:"index_pattern: { type: keyword }"`
 	TimeField    string `json:"time_field,omitempty" elastic_mapping:"time_field: { type: keyword }"`
-	Filter      interface{} `json:"filter,omitempty" elastic_mapping:"filter: { type: object }"`
+	Filter      interface{} `json:"filter,omitempty" elastic_mapping:"filter: { type: object, enabled:false }"`
 	BucketSize     string   `json:"bucket_size" elastic_mapping:"bucket_size: { type: keyword }"`
 	Title          string   `json:"title"  elastic_mapping:"title: { type: keyword }"`
 	Description    string   `json:"description" elastic_mapping:"description: { type: keyword }"`
-	Visualizations interface{} `json:"visualizations"  elastic_mapping:"visualizations: { type: object }"`
+	Visualizations interface{} `json:"visualizations"  elastic_mapping:"visualizations: { type: object, enabled:false }"`
 	Tags []string `json:"tags,omitempty" elastic_mapping:"tags: { type: keyword }"`
 	User string `json:"user" elastic_mapping:"user: { type: keyword }"`
-	Query interface{} `json:"query,omitempty"`
-	TimeFilter interface{} `json:"time_filter,omitempty"`
+	Query interface{} `json:"query,omitempty" elastic_mapping:"query: { type: object, enabled:false }"`
+	TimeFilter interface{} `json:"time_filter,omitempty" elastic_mapping:"time_filter: { type: object, enabled:false }"`
 }
