b239ddf60b9d8b3a8e49a1322c88e18a106c4775
sunjiacheng <sunjiacheng@infini.ltd>
2023-05-19 14:50:25 +0800

    [elastic] add MergePartitions (#321)
    [elastic] add MergePartitions
    
    Co-authored-by: Kassian Sun <kassiansun@outlook.com>

--- core/elastic/partition.go
+++ core/elastic/partition.go
@@ -6,50 +6,53 @@ package elastic
 
 import (
 	"fmt"
-	"github.com/buger/jsonparser"
-	"infini.sh/framework/core/util"
+	"math"
 	"net/http"
 	"strconv"
 	"strings"
+
+	"github.com/buger/jsonparser"
+	"infini.sh/framework/core/util"
 )
 
 type PartitionQuery struct {
-	IndexName string `json:"index_name"`
-	FieldType string `json:"field_type"`
-	FieldName string `json:"field_name"`
-	Step      interface{}    `json:"step"`
-	Filter interface{} `json:"filter"`
-	DocType string `json:"doc_type"`
+	IndexName string      `json:"index_name"`
+	FieldType string      `json:"field_type"`
+	FieldName string      `json:"field_name"`
+	Step      interface{} `json:"step"`
+	Filter    interface{} `json:"filter"`
+	DocType   string      `json:"doc_type"`
 }
 
 type PartitionInfo struct {
-	Start float64 `json:"start"`
-	End float64 `json:"end"`
+	Key    float64                `json:"key"`
+	Start  float64                `json:"start"`
+	End    float64                `json:"end"`
 	Filter map[string]interface{} `json:"filter"`
-	Docs int64 `json:"docs"`
-	Other bool
+	Docs   int64                  `json:"docs"`
+	Other  bool
 }
 
 type BoundValuesResult struct {
-	Min float64
-	Max float64
-	Null int64
+	Min             float64
+	Max             float64
+	Null            int64
 	NotExistsFilter map[string]interface{}
 }
 
 const (
-	PartitionByDate = "date"
+	PartitionByDate    = "date"
 	PartitionByKeyword = "keyword"
-	PartitionByNumber = "number"
+	PartitionByNumber  = "number"
 )
 
-func GetPartitions(q *PartitionQuery, client API)([]PartitionInfo, error){
+func GetPartitions(q *PartitionQuery, client API) ([]PartitionInfo, error) {
 	if q == nil {
 		return nil, fmt.Errorf("patition query can not be empty")
 	}
 	var (
 		vFilter interface{}
-		must []interface{}
+		must    []interface{}
 	)
 	if q.Filter != nil {
 		must = append(must, q.Filter)
@@ -77,14 +80,14 @@ func GetPartitions(q *PartitionQuery, client API)([]PartitionInfo, error){
 		if q.FieldType == PartitionByDate {
 			if stepV, ok := q.Step.(string); !ok {
 				return nil, fmt.Errorf("expect step value of string type since filedtype is %s", PartitionByDate)
-			}else{
+			} else {
 				du, err := util.ParseDuration(stepV)
 				if err != nil {
 					return nil, fmt.Errorf("parse step duration error: %w", err)
 				}
 				step = float64(du.Milliseconds())
 			}
-		}else {
+		} else {
 			switch q.Step.(type) {
 			case float64:
 				step = q.Step.(float64)
@@ -118,8 +121,8 @@ func GetPartitions(q *PartitionQuery, client API)([]PartitionInfo, error){
 		if result.Null > 0 {
 			partitions = append(partitions, PartitionInfo{
 				Filter: result.NotExistsFilter,
-				Other: true,
-				Docs: result.Null,
+				Other:  true,
+				Docs:   result.Null,
 			})
 		}
 		return partitions, nil
@@ -128,13 +131,13 @@ func GetPartitions(q *PartitionQuery, client API)([]PartitionInfo, error){
 	}
 }
 
-func getPartitionsByAgg(client API, indexName string, fieldName, fieldType string, step float64, filter interface{}) ([]PartitionInfo, error){
+func getPartitionsByAgg(client API, indexName string, fieldName, fieldType string, step float64, filter interface{}) ([]PartitionInfo, error) {
 	queryDsl := util.MapStr{
 		"size": 0,
 		"aggs": util.MapStr{
 			"partitions": util.MapStr{
 				"histogram": util.MapStr{
-					"field": fieldName,
+					"field":    fieldName,
 					"interval": step,
 				},
 				"aggs": util.MapStr{
@@ -167,8 +170,9 @@ func getPartitionsByAgg(client API, indexName string, fieldName, fieldType strin
 			continue
 		}
 		var (
-			min float64
-			max float64
+			min   float64
+			max   float64
+			key   float64
 			minOK bool
 			maxOK bool
 		)
@@ -178,46 +182,97 @@ func getPartitionsByAgg(client API, indexName string, fieldName, fieldType strin
 		if maxM, ok := bk["max"].(map[string]interface{}); ok {
 			max, maxOK = maxM["value"].(float64)
 		}
+		if keyM, ok := bk["key"].(float64); ok {
+			key = keyM
+		}
 		if minOK && maxOK {
 			partition := PartitionInfo{
+				Key:   key,
 				Start: min,
-				End: max,
-				Docs: int64(docCount),
+				End:   max,
+				Docs:  int64(docCount),
 				Other: false,
 			}
-			rv := util.MapStr{
-				"gte": min,
-				"lte": max,
-			}
-			if fieldType == PartitionByDate {
-				rv["format"] = "epoch_millis"
-			}
-
-			must := []interface{}{
-				util.MapStr{
-					"range": util.MapStr{
-						fieldName: rv,
-					},
-				},
-			}
-			if filter != nil {
-				must = append(must, filter)
-			}
-			partition.Filter = util.MapStr{
-				"bool": util.MapStr{
-					"must": must,
-				},
-			}
+			partition.Filter = buildPartitionFilter(min, max, fieldName, fieldType, filter)
 			partitions = append(partitions, partition)
 		}
 	}
 	return partitions, nil
 }
 
+// NOTE: we assume GetPartitions returned sorted buckets from ES, if not, we need to manually sort source & target partitions by keys
+// sourcePartitions & targetPartitions must've been generated with same bucket step & offset
+func MergePartitions(sourcePartitions []PartitionInfo, targetPartitions []PartitionInfo, fieldName, fieldType string, filter interface{}) []PartitionInfo {
+	sourceIdx, targetIdx := 0, 0
+	var ret []PartitionInfo
+	for {
+		if sourceIdx >= len(sourcePartitions) || targetIdx >= len(targetPartitions) {
+			break
+		}
+		source := sourcePartitions[sourceIdx]
+		target := targetPartitions[targetIdx]
+		if source.Key < target.Key {
+			ret = append(ret, source)
+			sourceIdx += 1
+			continue
+		}
+		if source.Key > target.Key {
+			ret = append(ret, target)
+			targetIdx += 1
+			continue
+		}
+		partition := PartitionInfo{
+			Key:   source.Key,
+			Start: math.Min(source.Start, target.Start),
+			End:   math.Max(source.End, target.End),
+			// NOTE: not accurate, don't use
+			Docs:  util.MaxInt64(source.Docs, target.Docs),
+			Other: false,
+		}
+		partition.Filter = buildPartitionFilter(partition.Start, partition.End, fieldName, fieldType, filter)
+		ret = append(ret, partition)
+		sourceIdx += 1
+		targetIdx += 1
+	}
+	for i := sourceIdx; i < len(sourcePartitions); i += 1 {
+		ret = append(ret, sourcePartitions[i])
+	}
+	for i := targetIdx; i < len(targetPartitions); i += 1 {
+		ret = append(ret, targetPartitions[i])
+	}
+	return ret
+}
+
+func buildPartitionFilter(min, max float64, fieldName, fieldType string, filter interface{}) util.MapStr {
+	rv := util.MapStr{
+		"gte": min,
+		"lte": max,
+	}
+	if fieldType == PartitionByDate {
+		rv["format"] = "epoch_millis"
+	}
+	must := []interface{}{
+		util.MapStr{
+			"range": util.MapStr{
+				fieldName: rv,
+			},
+		},
+	}
+	if filter != nil {
+		must = append(must, filter)
+	}
+	return util.MapStr{
+		"bool": util.MapStr{
+			"must": must,
+		},
+	}
+
+}
+
 func getBoundValues(client API, indexName string, fieldName string, filter interface{}) (*BoundValuesResult, error) {
 	nullFilter := util.MapStr{
 		"bool": util.MapStr{
-			"must_not":[]util.MapStr{
+			"must_not": []util.MapStr{
 				{
 					"exists": util.MapStr{
 						"field": fieldName,
@@ -252,8 +307,8 @@ func getBoundValues(client API, indexName string, fieldName string, filter inter
 		return nil, err
 	}
 	result := BoundValuesResult{
-		Min: -1,
-		Max: -1,
+		Min:  -1,
+		Max:  -1,
 		Null: -1,
 	}
 	if res.GetTotal() == 0 {
@@ -276,28 +331,28 @@ func getBoundValues(client API, indexName string, fieldName string, filter inter
 	return &result, nil
 }
 
-func GetIndexTypes( client API, indexName string) (map[string]interface{} , error) {
+func GetIndexTypes(client API, indexName string) (map[string]interface{}, error) {
 	version := client.GetMajorVersion()
-	if version >= 8{
+	if version >= 8 {
 		return map[string]interface{}{}, nil
 	}
 	return getIndexTypes(client, indexName)
 }
 
-func getIndexTypes( client API, indexName string) (map[string]interface{} , error) {
+func getIndexTypes(client API, indexName string) (map[string]interface{}, error) {
 	query := util.MapStr{
 		"size": 0,
-			"aggs": util.MapStr{
+		"aggs": util.MapStr{
 			"group_by_index": util.MapStr{
 				"terms": util.MapStr{
 					"field": "_index",
-					"size": 500,
+					"size":  500,
 				},
 				"aggs": util.MapStr{
 					"group_by_type": util.MapStr{
 						"terms": util.MapStr{
 							"field": "_type",
-								"size": 20,
+							"size":  20,
 						},
 					},
 				},
@@ -314,7 +369,7 @@ func getIndexTypes( client API, indexName string) (map[string]interface{} , erro
 	typeInfo := map[string]interface{}{}
 	if indexAggs, ok := searchRes.Aggregations["group_by_index"]; ok {
 		for _, bk := range indexAggs.Buckets {
-			if iname, ok := bk["key"].(string); ok{
+			if iname, ok := bk["key"].(string); ok {
 				info := map[string]interface{}{}
 				if typeAggs, ok := bk["group_by_type"].(map[string]interface{}); ok {
 					if bks, ok := typeAggs["buckets"].([]interface{}); ok {
