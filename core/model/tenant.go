15c4ecd9b9b4e6b49d269ba619e56e17a7be8b03
medcl <m@medcl.net>
2023-11-28 13:46:51 +0800

    update domain

--- core/model/tenant.go
+++ core/model/tenant.go
@@ -41,3 +41,13 @@ type TenantInfo struct {
 	ID   string `json:"id,omitempty"`
 	Name string `json:"name,omitempty"`
 }
+
+type TeamInfo struct {
+	ID   string `json:"id,omitempty"`
+	Name string `json:"name,omitempty"`
+}
+
+type ProjectInfo struct {
+	ID   string `json:"id,omitempty"`
+	Name string `json:"name,omitempty"`
+}
