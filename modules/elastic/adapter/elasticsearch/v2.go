ada960f5e2041c97da9bcc98d04acb023e4c3a13
medcl <m@medcl.net>
2024-05-07 10:15:56 +0800

    feat: use http body to pass scroll_id for next scroll fetch

--- modules/elastic/adapter/elasticsearch/v2.go
+++ modules/elastic/adapter/elasticsearch/v2.go
@@ -4,7 +4,12 @@ import (
 	"context"
 	"errors"
 	"fmt"
+	"infini.sh/framework/core/elastic"
+	"infini.sh/framework/core/global"
 	"infini.sh/framework/core/util"
+	"infini.sh/framework/modules/elastic/adapter"
+	log "github.com/cihub/seelog"
+	"time"
 )
 
 type ESAPIV2 struct {
@@ -24,4 +29,27 @@ func (s *ESAPIV2) ClearScroll(scrollId string) error {
 		return errors.New(string(resp.Body))
 	}
 	return nil
+}
+
+func (s *ESAPIV2) NextScroll(ctx *elastic.APIContext, scrollTime string, scrollId string) ([]byte, error) {
+
+	url := fmt.Sprintf("%s/_search/scroll", s.GetEndpoint())
+	body:=util.MapStr{}
+	body["scroll_id"]=scrollId
+	body["scroll"]=scrollTime
+
+	resp, err := adapter.RequestTimeout(ctx, util.Verb_POST, url, nil, s.metadata, time.Duration(s.metadata.Config.RequestTimeout)*time.Second)
+	if err != nil {
+		return nil, err
+	}
+
+	if resp.StatusCode != 200 {
+		return nil, errors.New(string(resp.Body))
+	}
+
+	if global.Env().IsDebug {
+		log.Trace("next scroll,", url, "m,", string(resp.Body))
+	}
+
+	return resp.Body, nil
 }
