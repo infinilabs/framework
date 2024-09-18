e261310c2367ee926238d7cd657971e9e0e63269
hardy <luohf@infinilabs.com>
2024-09-18 16:09:15 +0800

    fix: when file not exits continue delete (#614)
    Reviewed-on: https://git.infini.ltd/infini/framework/pulls/614
    Co-authored-by: hardy <luohf@infinilabs.com>
    Co-committed-by: hardy <luohf@infinilabs.com>

--- modules/queue/disk_queue/cleanup.go
+++ modules/queue/disk_queue/cleanup.go
@@ -5,57 +5,58 @@
 package queue
 
 import (
+	"os"
+
 	log "github.com/cihub/seelog"
 	"infini.sh/framework/core/global"
 	"infini.sh/framework/core/queue"
 	"infini.sh/framework/core/util"
-	"os"
 )
 
-func (module *DiskQueue)GetEarlierOffsetByQueueID(queueID string)(int,int64)  {
-	consumers, eSegmentNum, _,_ := queue.GetEarlierOffsetByQueueID(queueID)
-	q,ok:=module.queues.Load(queueID)
-	if ok{
-		var c=0
+func (module *DiskQueue) GetEarlierOffsetByQueueID(queueID string) (int, int64) {
+	consumers, eSegmentNum, _, _ := queue.GetEarlierOffsetByQueueID(queueID)
+	q, ok := module.queues.Load(queueID)
+	if ok {
+		var c = 0
 		(q.(*DiskBasedQueue)).consumersInReading.Range(func(key, value any) bool {
-			seg:=value.(int64)
+			seg := value.(int64)
 			c++
-			if seg<eSegmentNum{
-				if global.Env().IsDebug{
-					log.Debug(queueID,",",seg," < ",eSegmentNum, " use:",seg)
+			if seg < eSegmentNum {
+				if global.Env().IsDebug {
+					log.Debug(queueID, ",", seg, " < ", eSegmentNum, " use:", seg)
 				}
-				eSegmentNum=seg
+				eSegmentNum = seg
 			}
 			return true
 		})
-		if c>consumers{
-			consumers=c
+		if c > consumers {
+			consumers = c
 		}
 	}
-	return consumers,eSegmentNum
+	return consumers, eSegmentNum
 }
 
-func (module *DiskQueue)GetLatestOffsetByQueueID(queueID string)(int,int64)  {
+func (module *DiskQueue) GetLatestOffsetByQueueID(queueID string) (int, int64) {
 	consumers, eSegmentNum, _ := queue.GetLatestOffsetByQueueID(queueID)
-	q,ok:=module.queues.Load(queueID)
-	if ok{
-		var c=0
+	q, ok := module.queues.Load(queueID)
+	if ok {
+		var c = 0
 		(q.(*DiskBasedQueue)).consumersInReading.Range(func(key, value any) bool {
-			seg:=value.(int64)
+			seg := value.(int64)
 			c++
-			if seg>eSegmentNum{
+			if seg > eSegmentNum {
 				if global.Env().IsDebug {
 					log.Trace(queueID, ",", seg, " > ", eSegmentNum, " use:", seg)
 				}
-				eSegmentNum=seg
+				eSegmentNum = seg
 			}
 			return true
 		})
-		if c>consumers{
-			consumers=c
+		if c > consumers {
+			consumers = c
 		}
 	}
-	return consumers,eSegmentNum
+	return consumers, eSegmentNum
 }
 
 func (module *DiskQueue) deleteUnusedFiles(queueID string, fileNum int64) {
@@ -67,60 +68,57 @@ func (module *DiskQueue) deleteUnusedFiles(queueID string, fileNum int64) {
 	consumers, eSegmentNum := module.GetEarlierOffsetByQueueID(queueID)
 	fileStartToDelete := fileNum - module.cfg.Retention.MaxNumOfLocalFiles
 
-	if fileStartToDelete <= 0 || consumers <= 0|| eSegmentNum <0 {
+	if fileStartToDelete <= 0 || consumers <= 0 || eSegmentNum < 0 {
 		log.Debugf("queue: %v, no consumers or consumer/s3 already ahead of this file, %v, %v, %v", queueID, fileStartToDelete, consumers, eSegmentNum)
 		return
 	}
 
 	_, lSegmentNum := module.GetLatestOffsetByQueueID(queueID) //delete saved file to latest offset(keep 5 distance)
 
-
 	if module.cfg.UploadToS3 {
 		//check last uploaded mark
 		var lastSavedFileNum = GetLastS3UploadFileNum(queueID)
-		log.Trace("disk, delete ",queueID,",",fileNum,",",consumers,",", eSegmentNum,",",fileStartToDelete,",",lastSavedFileNum,fileStartToDelete >= lastSavedFileNum)
+		log.Trace("disk, delete ", queueID, ",", fileNum, ",", consumers, ",", eSegmentNum, ",", fileStartToDelete, ",", lastSavedFileNum, fileStartToDelete >= lastSavedFileNum)
 
-		if lastSavedFileNum<0{
+		if lastSavedFileNum < 0 {
 			return
 		}
 
 		if global.Env().IsDebug {
-			log.Tracef("disk, files start to delete:%v, consumer_on:%v, last_saved:%v", fileStartToDelete, eSegmentNum, lastSavedFileNum)
+			log.Debugf("disk, files start to delete:%v, consumer_on:%v, last_saved:%v", fileStartToDelete, eSegmentNum, lastSavedFileNum)
 		}
 
 		if fileStartToDelete >= lastSavedFileNum {
-			fileStartToDelete=lastSavedFileNum-module.cfg.Compress.IdleThreshold
+			fileStartToDelete = lastSavedFileNum - module.cfg.Compress.IdleThreshold
 		}
 
-		if  lastSavedFileNum - lSegmentNum > module.cfg.Compress.IdleThreshold{
+		if lastSavedFileNum-lSegmentNum > module.cfg.Compress.IdleThreshold {
 			log.Tracef("disk, files start to saved:%v, latest:%v", lastSavedFileNum, lSegmentNum)
 			//TODO foreach delete files
 		}
-
-
 	}
 
-	if eSegmentNum >0 && fileStartToDelete> eSegmentNum{
-		fileStartToDelete= eSegmentNum -module.cfg.Retention.MaxNumOfLocalFiles
+	if eSegmentNum > 0 && fileStartToDelete > eSegmentNum {
+		fileStartToDelete = eSegmentNum - module.cfg.Retention.MaxNumOfLocalFiles
 	}
 
 	//has consumers
-	if consumers > 0 && fileStartToDelete>0 && fileStartToDelete < eSegmentNum && eSegmentNum >0{
+	if consumers > 0 && fileStartToDelete > 0 && fileStartToDelete < eSegmentNum && eSegmentNum > 0 {
 		log.Trace(queueID, " start to delete:", fileStartToDelete, ",consumers:", consumers, ",consumer_on:", eSegmentNum)
 
 		//TODO do not wall all files, when numbers growing, it will be slow to check each file exists or not
 		for x := fileStartToDelete; x >= 0; x-- {
 
 			//if file greater than earliest segment, skip
-			if x> eSegmentNum  {
+			if x > eSegmentNum {
 				continue
 			}
 
 			file := GetFileName(queueID, x)
 			log.Trace(queueID, " start to delete:", file)
-			var exists=false
+			var exists = false
 			if util.FileExists(file) {
-				exists=true
+				exists = true
 				log.Debug("delete queue file:", file)
 				err := os.Remove(file)
 				if err != nil {
@@ -130,7 +128,7 @@ func (module *DiskQueue) deleteUnusedFiles(queueID string, fileNum int64) {
 			}
 			compressedFile := file + compressFileSuffix
 			if util.FileExists(compressedFile) {
-				exists=true
+				exists = true
 				log.Trace("delete compressed queue file:", compressedFile)
 				err := os.Remove(compressedFile)
 				if err != nil {
@@ -140,15 +138,14 @@ func (module *DiskQueue) deleteUnusedFiles(queueID string, fileNum int64) {
 			}
 
 			//no compress or flat file exists
-			if !exists{
-				log.Debug("skip further delete, missing queue file:", file)
-				return
+			if !exists {
+				log.Tracef("continue further delete, missing queue file:", file)
+				continue
 			}
-
 		}
 	} else {
 		//FIFO queue, need to delete old files
-		log.Debugf("skip delete, queue:%v, consumers:%v, fileID:%v, file start to delete:%v , segment num:%v",queueID,consumers,fileNum,fileStartToDelete, eSegmentNum)
+		log.Debugf("skip delete, queue:%v, consumers:%v, fileID:%v, file start to delete:%v , segment num:%v", queueID, consumers, fileNum, fileStartToDelete, eSegmentNum)
 		//check current read depth and file num
 	}
 
