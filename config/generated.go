2158b3d50fea3fecd03edb431b37b3d654dee7a2
hardy <luohoufu@163.com>
2025-01-13 14:04:32 +0800

    chore: restore-generated-file

--- config/generated.go
+++ config/generated.go
@@ -1,10 +1,10 @@
 package config
 
-const LastCommitLog = "6aeaa10bf4d2da3005441f483c71758b28fdd9ad"
-const BuildDate = "2025-01-13T04:25:09Z"
+const LastCommitLog = "N/A"
+const BuildDate = "N/A"
 
-const EOLDate  = "2023-12-31T10:10:10Z"
+const EOLDate = "N/A"
 
-const Version  = "1.0.0_SNAPSHOT"
+const Version = "0.0.1-SNAPSHOT"
 
-const BuildNumber  = "001"
+const BuildNumber = "001"
