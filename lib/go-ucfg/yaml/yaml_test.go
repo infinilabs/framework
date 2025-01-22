d82d1533762284c2b4dcff08089e63d8ca799693
hardy <luohf@infinilabs.com>
2025-01-22 11:35:08 +0800

    chore: add license header

--- lib/go-ucfg/yaml/yaml_test.go
+++ lib/go-ucfg/yaml/yaml_test.go
@@ -1,19 +1,25 @@
-// Licensed to Elasticsearch B.V. under one or more contributor
-// license agreements. See the NOTICE file distributed with
-// this work for additional information regarding copyright
-// ownership. Elasticsearch B.V. licenses this file to you under
-// the Apache License, Version 2.0 (the "License"); you may
-// not use this file except in compliance with the License.
-// You may obtain a copy of the License at
+// Copyright (C) INFINI Labs & INFINI LIMITED.
 //
-//     http://www.apache.org/licenses/LICENSE-2.0
+// The INFINI Console is offered under the GNU Affero General Public License v3.0
+// and as commercial software.
 //
-// Unless required by applicable law or agreed to in writing,
-// software distributed under the License is distributed on an
-// "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
-// KIND, either express or implied.  See the License for the
-// specific language governing permissions and limitations
-// under the License.
+// For commercial licensing, contact us at:
+//   - Website: infinilabs.com
+//   - Email: hello@infini.ltd
+//
+// Open Source licensed under AGPL V3:
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU Affero General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+// GNU Affero General Public License for more details.
+//
+// You should have received a copy of the GNU Affero General Public License
+// along with this program. If not, see <http://www.gnu.org/licenses/>.
 
 package yaml
 
@@ -246,7 +252,7 @@ var (
 )
 
 func TestDecodeDefaultCfg(t *testing.T) {
-	path := "patch_test.yml"
+	path := "yaml_test.yml"
 	in, err := os.ReadFile(path)
 	if err != nil {
 		t.Fatalf("Failed to read config file: %v", err)
@@ -263,7 +269,7 @@ func TestDecodeDefaultCfg(t *testing.T) {
 }
 
 func TestLoadDefaultCfg(t *testing.T) {
-	path := "patch_test.yml"
+	path := "yaml_test.yml"
 	appConfig := defaultConfig // copy default config so it's not overwritten
 	config, err := NewConfigWithFile(path, ucfg.PathSep("."))
 	if err != nil {
