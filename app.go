79ecbde5352393407c57b1fee4a4422b51609942
hardy <luohf@infinilabs.com>
2025-04-15 09:49:32 +0800

    chore: set default `RestartSec` service option to 120 seconds

--- app.go
+++ app.go
@@ -570,6 +570,7 @@ func (app *App) Run() {
 	//init service
 	svcOptions := make(service.KeyValue)
 	svcOptions["Restart"] = "always"
+	svcOptions["RestartSec"] = 120
 	svcOptions["SuccessExitStatus"] = "1 2 8 SIGKILL"
 	svcOptions["LimitNOFILE"] = 1024000
 
