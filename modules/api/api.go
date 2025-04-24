31b1b90c41b7ba1de11c5ba36b7113d5fcce947d
medcl <m@medcl.net>
2025-04-24 11:17:59 +0800

    chore: add info api to ui module, need login to access

--- modules/api/api.go
+++ modules/api/api.go
@@ -63,6 +63,8 @@ func init() {
 	api.HandleAPIMethod(api.GET, "/_version", versionAPIHandler)
 	api.HandleAPIMethod(api.GET, "/_info", infoAPIHandler)
 	api.HandleAPIMethod(api.GET, "/health", healthAPIHandler)
+
+	api.HandleUIMethod(api.GET, "/_info", infoAPIHandler,api.RequireLogin())
 }
 
 func whoisAPIHandler(w http.ResponseWriter, req *http.Request, ps httprouter.Params) {
