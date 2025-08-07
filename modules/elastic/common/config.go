4c78b1e31ac8c5df1bda2d2de6dda612ac8b50a8
Medcl <m@medcl.net>
2025-08-07 17:36:15 +0800

    refactor: refactoring schema init logic, disable index based template… (#188)
    * refactor: refactoring schema init logic, disable index based template by default
    
    * chore: update docs

--- modules/elastic/common/config.go
+++ modules/elastic/common/config.go
@@ -45,6 +45,8 @@ type ORMConfig struct {
 	InitTemplate            bool   `config:"init_template"`
 	SkipInitDefaultTemplate bool   `config:"skip_init_default_template"`
 	OverrideExistsTemplate  bool   `config:"override_exists_template"`
+	BuildTemplateForObject  bool   `config:"build_template_for_object"`
+	PanicOnInitSchemaError  bool   `config:"panic_on_init_schema_error"`
 	TemplateName            string `config:"template_name"` //default template name
 
 	InitSchema  bool   `config:"init_schema"`
