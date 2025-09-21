e5c9febb05eec9dd8d41ab5dd5cae204c362a150
Medcl <m@medcl.net>
2025-09-21 10:42:31 +0800

    refactor: refactoring query string parser (#216)
    * refactor: refactoring query string parser
    
    * chore: update docs
    
    * chore: fix nil exception
    
    * chore: check content lenght first
    
    ---------
    
    Co-authored-by: silenceqi <silenceqi@hotmail.com>

--- core/api/handler.go
+++ core/api/handler.go
@@ -335,22 +335,28 @@ func (handler Handler) DecodeJSON(r *http.Request, o interface{}) error {
 	return json.Unmarshal(content, o)
 }
 
+func ReadBody(r *http.Request) ([]byte, error) {
+	if r.ContentLength > 0 && r.Body != nil {
+		content, err := ioutil.ReadAll(r.Body)
+		_ = r.Body.Close()
+		if err != nil {
+			return nil, err
+		}
+		if len(content) == 0 {
+			return nil, errors.NewWithCode(err, errors.BodyEmpty, r.URL.String())
+		}
+
+		// Replace r.Body so it can be read again later
+		r.Body = ioutil.NopCloser(bytes.NewBuffer(content))
+
+		return content, nil
+	}
+	return nil, errors.Error("request body is nil")
+}
+
 // GetRawBody return raw http request body
 func (handler Handler) GetRawBody(r *http.Request) ([]byte, error) {
-
-	content, err := ioutil.ReadAll(r.Body)
-	_ = r.Body.Close()
-	if err != nil {
-		return nil, err
-	}
-	if len(content) == 0 {
-		return nil, errors.NewWithCode(err, errors.BodyEmpty, r.URL.String())
-	}
-
-	// Replace r.Body so it can be read again later
-	r.Body = ioutil.NopCloser(bytes.NewBuffer(content))
-
-	return content, nil
+	return ReadBody(r)
 }
 
 // Write response to client
