56db72c1db7f080555eb9fb488cc152f9cb88ef1
silenceqi <silenceqi@hotmail.com>
2025-12-17 14:37:58 +0800

    feat: add avatar configuration for principal (#246)

--- core/security/principal.go
+++ core/security/principal.go
@@ -14,6 +14,7 @@ type OrganizationPrincipal struct {
 	Type        string `json:"type,omitempty" elastic_mapping:"type:{type:keyword}"` //  "type": "user", // or "group"
 	Name        string `json:"name,omitempty" elastic_mapping:"name:{type:keyword,copy_to:combined_fulltext}"`
 	Description string `json:"description,omitempty" elastic_mapping:"description:{type:keyword,copy_to:combined_fulltext}"`
+	Avatar  string `json:"avatar,omitempty" elastic_mapping:"avatar:{type:keyword}"`
 }
 
 const PrincipalTypeUser = "user"
