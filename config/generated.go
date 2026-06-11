615ca023016f616b462b26e66259aa1be7bce6c1
hardy <luohf@infinilabs.com>
2026-06-11 17:36:26 +0800

    chore: revert the generated info

--- config/generated.go
+++ config/generated.go
@@ -1,11 +1,11 @@
 package config
 
-const LastCommitLog = "2fb02a59a5662cb10782d889261d5e9eb7db7243"
+const LastCommitLog = "N/A"
 
-const BuildDate = "2026-06-11T09:27:13Z"
+const BuildDate = "N/A"
 
-const EOLDate  = "2023-12-31T10:10:10Z"
+const EOLDate = "N/A"
 
-const Version  = "1.0.0_SNAPSHOT"
+const Version = "0.0.1-SNAPSHOT"
 
-const BuildNumber  = "001"
+const BuildNumber = "001"
